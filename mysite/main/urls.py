b8b9a764a76aa36b074bd9264b74ad15123b73bf
AlbertZaqaryan <itm.albert.zaqaryan@gmail.com>
2023-03-31 18:06:15 +0400

    v2

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from . import views
+from . import views 
+
 
 urlpatterns=[
     path('', views.index, name='index'),
     path('brand/<int:id>/', views.index_detail, name='index_detail'),
+    path('contact/', views.contact, name='contact'), 
+    path('login/', views.login_request, name='login'), 
+    path('register/', views.register_request, name='register'), 
+    path('logout/', views.logout_request, name='logout'), 
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import HomeSlider, HomeSliderActive, Category, SubCategory
-# Create your views here.
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import NewUserForm
 
 
 def index(request):
@@ -12,7 +15,8 @@ def index(request):
         'tertox_slider':tertox_slider,
         'mnacac_sliderner':mnacac_sliderner,
         'category_list':category_list,
-        'sub_category_list':sub_category_list
+        'sub_category_list':sub_category_list,
+	    'luys':'index'
 
     })
 
@@ -30,3 +34,47 @@ def index_detail(request, id):
         'category_list_count':category_list_count
 
     })
+
+def contact(request):
+    return render(request,"main/contact-us.html", context={
+	    'luys':'contact'
+    })
+
+
+
+def register_request(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registration successful." )
+			return redirect("index")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm()
+	return render(request=request, template_name="main/register.html", context={"register_form":form})
+
+def login_request(request):
+	error = ''
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}.")
+				return redirect("index")
+			else:
+				messages.error(request,"Invalid username or password.")
+		else:
+			error = "Invalid username or password."
+			messages.error(request,"Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="main/login.html", context={"login_form":form, 'error':error})
+
+def logout_request(request):
+	logout(request)
+	messages.info(request, "You have successfully logged out.") 
+	return redirect("index")
